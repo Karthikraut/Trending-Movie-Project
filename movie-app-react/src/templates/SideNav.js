3b3b86e67ff569a89715126eb3948e5be384be1f
Karthikraut <karthikraut2@gmail.com>
2024-05-02 10:56:08 +0530

    SideNav Has been Developed.

---
+++ movie-app-react/src/templates/SideNav.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+const SideNav = () => {
+  return (
+    <div className='w-[20%] h-full  border-r-2 border-zinc-400 p-3'>
+        <h1 className='text-white font-bold'>
+        <i className="text-[#6556CD] ri-tv-fill text-2xl mr-2"></i>
+            <span className='text-2xl'> FilmFusion</span>
+        </h1>
+        <nav className='flex flex-col text-zinc-400 text-md gap-3 text-lg'>
+            <h1 className='text-white font-semibold mt-10 mb-5'>New Feed</h1>
+            <Link to={"/trending"} className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5">
+                <i class="ri-fire-fill"></i> Trending
+            </Link>
+            <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5">
+            <i class="ri-bard-fill"></i> Popular
+            </Link>
+            <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5">
+            <i class="ri-movie-2-fill"></i> Movies
+            </Link>
+            <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5">
+            <i class="ri-tv-2-fill"></i> Tv Shows
+            </Link>
+            <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5">
+            <i class="ri-team-fill"></i> People
+            </Link>
+        </nav>
+        <hr className='border-none h-[1px] bg-zinc-400 '/>
+        <nav className='flex flex-col text-zinc-400 text-md gap-3 text-lg'>
+            <Link to={"/trending"} className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5">
+                <i class="ri-information-fill"></i> About
+            </Link>
+            <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5">
+            <i class="ri-phone-fill"></i> Contact
+            </Link>
+        </nav>
+    </div>
+  )
+}
+
+export default SideNav;
---
+++ movie-app-react/src/templates/TopNav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TopNav = () => {
+  return (
+    <div className='w-full bg-red-100 h-[10vh] relative'>
+        <i className="text-3xl text-zinc-400 ri-search-line"></i>
+
+    </div>
+  )
+}
+
+export default TopNav;
